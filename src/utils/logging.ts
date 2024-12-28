f991f6a3b8a12213cac8b8f1e0c12e695407f480
Muhammad Quraisy <quraisy@uai.ac.id>
2024-12-28 10:21:37 +0700

    refactor: update console transport format

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,13 +1,20 @@
 import winston from "winston";
 
+const consoleTransport = new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+});
+
 const transports =
   process.env.NODE_ENV === "production"
     ? [
-        new winston.transports.Console(),
+        consoleTransport,
         new winston.transports.File({ filename: "error.log", level: "error" }),
         new winston.transports.File({ filename: "combined.log" }),
       ]
-    : [new winston.transports.Console()];
+    : [consoleTransport];
 
 const logging = winston.createLogger({
   level: "info",
