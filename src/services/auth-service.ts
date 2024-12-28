b0c28110c2ed6193ccd4835674d0658f21db9a25
Muhammad Quraisy <quraisy@uai.ac.id>
2024-12-28 10:39:05 +0700

    feat(auth): encrypt new registered user password with bcrypt

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,6 +3,7 @@ import { AuthSchema } from "../schema/auth-schema";
 import { RegisterRequest } from "../types";
 import database from "../utils/database";
 import { Validation } from "../utils/validation";
+import bcrypt from "bcrypt";
 
 export class AuthService {
   static async register(request: RegisterRequest) {
@@ -18,11 +19,12 @@ export class AuthService {
       throw new ResponseError(400, "Email is already registered");
     }
 
+    const hashedPassword = await bcrypt.hash(registerRequest.password, 10);
     const newUser = await database.user.create({
       data: {
         name: registerRequest.name,
         email: registerRequest.email,
-        password: registerRequest.password,
+        password: hashedPassword,
       },
       select: {
         id: true,
