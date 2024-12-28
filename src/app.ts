fbf37c1d50704f752d63c0295f7b9e181fa51fae
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-12-29 03:35:13 +0700

    feat(cors): add CORS middleware and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import errorMiddleware from "./middleware/error-middleware";
 import authRoute from "./routes/auth-route";
 import userRoute from "./routes/user-route";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(authRoute);
 app.use(userRoute);
