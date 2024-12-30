bab7a08fbd73c658d030567419b6366c819441f1
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-12-30 19:34:06 +0700

    feat(middleware): enhance error handling to include session expiration for JWT

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { ZodError } from "zod";
 import { ResponseError } from "../models/response-error";
-import { JsonWebTokenError } from "jsonwebtoken";
+import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 export default function errorMiddleware(
   err: Error,
@@ -21,6 +21,12 @@ export default function errorMiddleware(
   if (err instanceof JsonWebTokenError) {
     return res.status(401).json({
       error: err.message,
+    });
+  }
+
+  if (err instanceof TokenExpiredError) {
+    return res.status(401).json({
+      error: "Session expired",
     });
   }
 
